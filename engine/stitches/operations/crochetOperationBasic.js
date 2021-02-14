59209e27b0feae017db542c70366cb7ce6bfb472
Jacek Galkowski <38131406+trekski@users.noreply.github.com>
2021-02-14 18:07:29 +0100

    proper geomatry calc. for base crochet operation

--- engine/stitches/operations/crochetOperationBasic.js
+++ engine/stitches/operations/crochetOperationBasic.js
@@ -6,25 +6,55 @@ class crochetOperationBasic extends crochetOperation {
     static getCommandName() {return "mk"}
     static minParams() {return 2}
 
-    static CALC_DEF_NEW_POS() {
-        return [10,10]
+    static CALC_DEF_NEW_POS(fromNode, byLinkType) {
+
+        //setup
+        let len = crochetOperation.linkFactory.getClass(byLinkType).getDefLen();
+        let start = fromNode.getVector();
+
+        // initial direction of the new position is relative to fromNode
+        // pointing away from center of mass of its neighbors
+        let neighbors = fromNode.getNeighborNodes(); // by def. fromnode will be excluded
+        if (neighbors.length==0) neighbors=[fromNode];
+        let neighborVecs = neighbors.map(e=>e.getVector())
+        let avgNeighborVec = vec2d.SUM(neighborVecs).scale(1/neighborVecs.length);
+        let delta = start.sub(avgNeighborVec);
+
+        // fix corner cases
+        if (delta.len()>0) {
+            // delta<>(0,0) => neighbors' CoM is not same as fromNode => go in opposite dir to it
+            delta = delta.len(len);
+            // CoM same as fromNode => extend the fromnode position outwards but turn by PI/20
+            if (neighbors[0]==fromNode) {delta = delta.rot(Math.PI/20)};
+        } else {
+            // delta == (0,0), then
+            // default direction is UP
+            //unless start node was off-center by a enough, then follow that
+            let dir = (start.len() < 0.01) ? new vec2d(0,-1) : start.unit().rot(Math.PI/20);
+            delta = dir.len(len);
+        }
+
+        return start.add(delta);
+
+  }
     }
 
     exec(){
-        let newLinkType = this.params[0]
-        let newNodeType = this.params[1]
-        let newSubject = this.subject.copy()
-        let source = this.subject.needleStack.pop();
-        let newNode, newLink, newPos
 
-        newPos = crochetOperationBasic.CALC_DEF_NEW_POS(source, newLinkType) // ??? where to put this function best ???
+        let newSubject, sourceNode, newNodeType, newNode, newLinkType, newLink, newPos
+
+        [newLinkType, newNodeType] = this.params[0]
+        newSubject = this.subject.copy()
+        sourceNode = newSubject.needleStack.pop();
+
+        newPos = crochetOperationBasic.CALC_DEF_NEW_POS(sourceNode, newLinkType) // ??? where to put this function best ???
         newNode = crochetOperation.nodeFactory.getNewObject(newNodeType, this.subject.stitch, newPos);
-        newLink = crochetOperation.linkFactory.getNewObject(newLinkType, this.subject.stitch, source, newNode);
-
+        newLink = crochetOperation.linkFactory.getNewObject(newLinkType, this.subject.stitch, sourceNode, newNode);
         newSubject.needleStack.push(newNode);
 
         let res = this.getBasicResult(newSubject, newNode, newLink)
         return res
+
   }
 
 }
