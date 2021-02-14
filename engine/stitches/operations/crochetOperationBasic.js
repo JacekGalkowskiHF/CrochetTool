f2447a694e5408a671d859b7d9f2c688b616bc3b
Jacek Galkowski <38131406+trekski@users.noreply.github.com>
2021-02-14 17:29:13 +0100

    .

--- engine/stitches/operations/crochetOperationBasic.js
+++ engine/stitches/operations/crochetOperationBasic.js
@@ -9,9 +9,23 @@ class crochetOperationBasic extends crochetOperation {
         let newLinkType = this.params[0]
         let newNodeType = this.params[1]
         let source = this.subject.needleStack.pop();
-        let newPos = crochetOperationBasic.CALC_DEF_NEW_POS(source, newLinkType) // ??? where to put this function best ???
-        let newNode = crochetOperation.nodeFactory.getNewObject(newNodeType, this.subject.stitch, newPos);
-        let newLink = crochetOperation.linkFactory.getLink(newLinkType, this.subject.stitch, source, newNode);
+        let newNode, newLink, newPos
+
+        try {
+            newPos = crochetOperationBasic.CALC_DEF_NEW_POS(source, newLinkType) // ??? where to put this function best ???
+        } catch(e) {
+            thow`crochetOperationBasic : failed to calualte new node position. Reason: ${e}`
+        }
+        try {
+            newNode = crochetOperation.nodeFactory.getNewObject(newNodeType, this.subject.stitch, newPos);
+        } catch(e) {
+            thow`crochetOperationBasic : failed to create new node '${newNodeType}'. Reason: ${e}`
+        }
+        try {
+            newLink = crochetOperation.linkFactory.getLink(newLinkType, this.subject.stitch, source, newNode);
+        } catch(e) {
+            thow`crochetOperationBasic : faield to create new link '${newLinkType}'. Reason: ${e}`
+        }
 
         this.subject.needleStack.push(target);
 
